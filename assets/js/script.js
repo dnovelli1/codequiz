b7da73151c94c82fea6c4cea8085141475d67ad8
Jake Novelli <jakenovelli11@gmail.com>
2021-03-27 16:57:03 -0700

    added pseudo code to script file

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+// <!-- Start with a screen that explains the game and contains a start button to intiate. -->
+// <!-- On this start screen will also be a high score link -->
+// <!-- Set a timer to begin when the start button is pressed, also taking you to the first question -->
+// <!-- Set the timer to begin at whatever time and decrement by 1 second -->
+// <!-- User will have 120 seconds to answer 10 questions -->
+// <!-- create a string of 10 questions that the user will go through -->
+// <!-- create 4 options for answers, adding a click to each one -->
+// <!-- Use if statements to determine if the answer is true, or else its false -->
+// <!-- Input text after button clicked that will display correct or incorrect. Add green text for correct and red for incorrect. -->
+// <!-- If false, the answer is incorrect and decrement the timer by 10 seconds -->
+// <!-- Keep track of questions answered correct, decrement by one for each incorrect -->
+// <!-- Create end game screen  -->
+// <!-- Display score by showing result of dividing questions answered by questions answered correctly. Thus showing a percentage -->
+// <!-- display input text box for user to enter their initials to submit to high score sheet -->
+// <!-- Store the high scores inside of a seperate html -->
+// <!-- Create play again button to restart the game -->
+// <!--  -->
